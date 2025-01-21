a0fef991a6bbb311896f1297f11c06882dc822dd
itLinux <itlinux@itLinux.lightspeed.brhmal.sbcglobal.net>
2025-01-21 13:39:36 +0330

    new from imann6889

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-  @Get('detail')
-  getUser(): string {
-    return this.usersService.getHi();
+  constructor(private readonly usersService: UsersService) { }
+
+  @Get()
+  index(): object {
+    return this.usersService.getAll()
+  }
+
+  @Get("/:id")
+  get(@Param('id') id: string) {
+    return this.usersService.getById(parseInt(id))
+  }
+
+  @Post()
+  store(@Body() data: object) {
+    return this.usersService.createUser(data)
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,52 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  getHi(): string {
-    return 'Hi!';
+
+  private users: any[] = [
+    {
+      id: 1,
+      username: "iman"
+    },
+    {
+      id: 2,
+      username: "Hossein"
+    },
+    {
+      id: 3,
+      username: "morteza"
+    },
+  ]
+
+  getAll(): object {
+    return {
+      data: this.users,
+      statusCode: 200,
+      message: "Get All users"
+    }
   }
+
+  getById(id: number): object {
+    let findUser = null;
+    for (const user of this.users) {
+      if (user.id == id)
+        findUser = user
+    }
+    return {
+      data: findUser,
+      statusCode: 200,
+      message: "Get User!"
+    }
+  }
+
+  createUser(data: object): object {
+    if (data) {
+      this.users.push(data)
+    }
+    return {
+      data: this.users,
+      statusCode: 200,
+      message: "created user!"
+    }
+  }
+
 }
